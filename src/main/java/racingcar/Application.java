69de8c57ce2e84a5421cd30c9d13251fbe67ee8a
Junbeom <warbringer98@gmail.com>
2025-10-27 23:58:21 +0900

    Feat: 입/출력 전 메세지 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,7 @@ public class Application {
         OutputView outputView = new OutputView();
 
         // 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNames = inputView.inputCars(inputView.readCars());
         List<Car> cars = new ArrayList<>();
         for (String name : carNames) {
@@ -16,11 +17,13 @@ public class Application {
         }
 
         // 라운드 입력
+        System.out.println("시도할 횟수는 몇 회인가요?");
         int roundCount = inputView.inputRoundCount((inputView.readRoundCount()));
 
         RacingGame racingGame = new RacingGame(cars);
 
         // 라운드 진행
+        System.out.println("\n실행 결과");
         for (int i = 0; i < roundCount; i++) {
             racingGame.playOneRound();
             outputView.printRoundResult(cars);
