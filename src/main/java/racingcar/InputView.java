e19b4b95359e9dd59ef924f4da233c45c0a365c5
Junbeom <warbringer98@gmail.com>
2025-10-27 22:39:38 +0900

    Fix: 실제 입력 받는 코드 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,6 +8,10 @@ import java.util.List;
 public class InputView {
 
     private static final String DELIMITER = ",";
+
+    public String readCars() {
+        return Console.readLine();
+    }
 
     public List<String> inputCars(String input) {
         List<String> names = trimString(input);
@@ -24,6 +29,10 @@ public class InputView {
 
         String[] parts = string.split(DELIMITER, -1);
         return new ArrayList<>(Arrays.asList(parts));
+    }
+
+    public String readRoundCount() {
+        return Console.readLine();
     }
 
     public int inputRoundCount(String input) {
