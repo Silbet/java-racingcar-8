949bc38e947ed6a9f5752448fab9a3c7de32a03f
Junbeom <warbringer98@gmail.com>
2025-10-27 19:24:32 +0900

    Test: 랜덤값에 의한 자동차 전진/정지 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -29,6 +31,28 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 랜덤숫자가_4이상이면_전진한다() {
+        Car car = new Car("qwer");
+        RacingGame game = new RacingGame();
+        int randomNumber = 4;
+
+        game.moveCarRandomly(car, randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 랜덤숫자가_4미만이면_정지한다() {
+        Car car = new Car("qwer");
+        RacingGame game = new RacingGame();
+        int randomNumber = 3;
+
+        game.moveCarRandomly(car, randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @Override
