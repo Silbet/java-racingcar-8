ca9d6040f6c41004383bf0d652bd1917a3de3c27
Junbeom <warbringer98@gmail.com>
2025-10-27 23:14:20 +0900

    Comment: 필요 없는 주석 제거

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -19,7 +19,6 @@ public class RacingGame {
         }
     }
 
-    // TODO: 차를 랜덤값에 이동시키는 것이 어디 클래스에 있는게 좋을까?
     void moveCarRandomly(Car car, int randomNumber) {
         if (randomNumber >= MOVE_FORWARD_STANDARD) {
             car.moveForward();
