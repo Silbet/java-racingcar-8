594d258c0a4ad6d08dedf54def713a2c8298267b
Junbeom <warbringer98@gmail.com>
2025-10-27 22:29:50 +0900

    Feat: 우승자 판별하는 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,11 +1,16 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class RacingGame {
     private List<Car> cars;
     private static final int MOVE_FORWARD_STANDARD = 4;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
 
     public void playRacingGame() {
         runRound();
@@ -25,5 +30,21 @@ public class RacingGame {
         }
     }
 
+    public List<String> getWinner() {
+        List<String> winners = new ArrayList<>();
+        int max = -1;
+
+        for (Car car : this.cars) {
+            int p = car.getPosition();
+            if (p > max) {
+                max = p;
+                winners.clear();
+                winners.add(car.getName());
+            } else if (p == max) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
 
 }
